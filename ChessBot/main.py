47a44a1db28cc17a5a0029d10fbcc5b5d61f3f17
YosefGilbert <148716471+YosefGilbert@users.noreply.github.com>
2024-12-04 17:04:17 -0500

    Updated main.py

--- ChessBot/main.py
+++ ChessBot/main.py
@@ -17,39 +17,18 @@
 
 from constraints import Board
 import constraints
+import misc
 import time
 from datetime import timedelta
 from gametree import GameTree
 
 
-def FENtoArr(fen_string):
-    board = []
-    for i in range(0, 8):
-        row = []
-        for j in range(0, 8):
-            row.append("0")
-        board.append(row)
-    curr_row = 0
-    curr_col = 0
-    for char in fen_string:
-        if not char.isdigit() and char != "/":
-            board[curr_row][curr_col] = char
-            curr_col += 1
-        if char.isdigit():
-            curr_col += int(char)
-        if char == "/":
-            curr_col = 0
-            curr_row += 1
-    return board
-
-
 if __name__ == '__main__':
     fen = input()
-    start_position = FENtoArr(fen)
-    starttime = time.perf_counter()
+    start_position = misc.FENtoArr(fen)
+    misc.playComputer()
 
-    tree = GameTree(start_position, 3)
-    tree.find_best_move()
+
 
 
 
